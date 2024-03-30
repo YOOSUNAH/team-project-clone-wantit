28c999f7eee5d3595fe408a048c18e6cee8cc731
dbfjru <dbfjru@gmail.com>
2024-03-30 13:15:29 +0900

    ✨ feat : 경매상품 단건 조회 API 구현

--- src/main/java/io/dcns/wantitauction/domain/auctionItem/service/AuctionItemService.java
+++ src/main/java/io/dcns/wantitauction/domain/auctionItem/service/AuctionItemService.java
@@ -1,5 +1,8 @@
 package io.dcns.wantitauction.domain.auctionItem.service;
 
+import io.dcns.wantitauction.domain.auctionItem.dto.AuctionItemResponseDto;
+import io.dcns.wantitauction.domain.auctionItem.entity.AuctionItem;
+import io.dcns.wantitauction.domain.auctionItem.repository.AuctionItemRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -7,4 +10,13 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class AuctionItemService {
 
+    private final AuctionItemRepository auctionItemRepository;
+
+    public AuctionItemResponseDto getProduct(Long auctionItemId) {
+        AuctionItem auctionItem = auctionItemRepository.findById(auctionItemId).orElseThrow(
+            () -> new IllegalArgumentException("존재하지 않는 경매상품 입니다.")
+        );
+
+        return new AuctionItemResponseDto(auctionItem);
+    }
 }
