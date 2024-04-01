1baab9b9e7f0fbed84477773bdae2d2dc9489fe4
dbfjru <dbfjru@gmail.com>
2024-04-01 20:59:44 +0900

    ✨ feat : 낙찰 상품 목록/단건 조회 API 구현

--- src/main/java/io/dcns/wantitauction/domain/auctionItem/repository/AuctionItemQueryRepository.java
+++ src/main/java/io/dcns/wantitauction/domain/auctionItem/repository/AuctionItemQueryRepository.java
@@ -1,9 +1,13 @@
 package io.dcns.wantitauction.domain.auctionItem.repository;
 
+import static io.dcns.wantitauction.domain.auctionItem.entity.QAuctionItem.auctionItem;
+
+import com.querydsl.core.types.Projections;
 import com.querydsl.jpa.impl.JPAQueryFactory;
-import io.dcns.wantitauction.domain.auctionItem.dto.MyAuctionItemsResponseDto;
-import io.dcns.wantitauction.domain.user.entity.User;
+import io.dcns.wantitauction.domain.auctionItem.dto.FinishedItemResponseDto;
+import io.dcns.wantitauction.domain.auctionItem.entity.AuctionItemEnum;
 import java.util.List;
+import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
@@ -13,8 +17,40 @@ public class AuctionItemQueryRepository {
 
     private final JPAQueryFactory jpaQueryFactory;
 
-    public List<MyAuctionItemsResponseDto> findFinishedAuctionItems(User user) {
-        // TODO : 내가 낙찰받은 물품 조회
-        return null;
+    public List<FinishedItemResponseDto> findAllByFinished() {
+        return jpaQueryFactory
+            .select(Projections.fields(FinishedItemResponseDto.class,
+                auctionItem.auctionItemId,
+                auctionItem.userId,
+                auctionItem.itemName,
+                auctionItem.productDescription,
+                auctionItem.minPrice,
+                auctionItem.maxPrice,
+                auctionItem.startDate,
+                auctionItem.endDate,
+                auctionItem.status))
+            .from(auctionItem)
+            .where(auctionItem.status.eq(AuctionItemEnum.FINISHED))
+            .fetch();
+    }
+
+    public Optional<FinishedItemResponseDto> findByIdAndFinished(Long auctionItemId) {
+        return Optional.ofNullable(
+            jpaQueryFactory
+                .select(Projections.fields(FinishedItemResponseDto.class,
+                    auctionItem.auctionItemId,
+                    auctionItem.userId,
+                    auctionItem.itemName,
+                    auctionItem.productDescription,
+                    auctionItem.minPrice,
+                    auctionItem.maxPrice,
+                    auctionItem.startDate,
+                    auctionItem.endDate,
+                    auctionItem.status))
+                .from(auctionItem)
+                .where(auctionItem.auctionItemId.eq(auctionItemId)
+                    .and(auctionItem.status.eq(AuctionItemEnum.FINISHED)))
+                .fetchFirst()
+        );
     }
 }
