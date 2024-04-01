73be6f85cd46e93ec0bc286b69a0526f7530726f
DBuu_Kong <feel00k0810@gmail.com>
2024-04-01 20:31:06 +0900

    ♻️ refactor : 데이터 포맷 변경

--- src/main/java/io/dcns/wantitauction/domain/auctionItem/dto/CreateProductRequestDto.java
+++ src/main/java/io/dcns/wantitauction/domain/auctionItem/dto/CreateProductRequestDto.java
@@ -1,5 +1,7 @@
 package io.dcns.wantitauction.domain.auctionItem.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonFormat.Shape;
 import io.dcns.wantitauction.domain.auctionItem.entity.AuctionItemEnum;
 import java.time.LocalDateTime;
 import lombok.Getter;
@@ -7,13 +9,17 @@ import lombok.Getter;
 @Getter
 public class CreateProductRequestDto {
 
-    private Long userId;
     private String itemName;
     private String productDescription;
     private Long minPrice;
     private Long maxPrice;
+
+    @JsonFormat(shape = Shape.STRING, pattern = "yyyy-MM-dd HH:mm", timezone = "Asia/Seoul")
     private LocalDateTime startDate;
+
+    @JsonFormat(shape = Shape.STRING, pattern = "yyyy-MM-dd HH:mm", timezone = "Asia/Seoul")
     private LocalDateTime endDate;
+
     private final AuctionItemEnum status = AuctionItemEnum.READY;
 
 }
