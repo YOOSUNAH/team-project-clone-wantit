b735da30b2db77cd82c67350dc3995db52fe9037
DBuu_Kong <feel00k0810@gmail.com>
2024-04-01 14:37:21 +0900

    🔧 chore : for -> stream 으로 로직 변경

--- src/main/java/io/dcns/wantitauction/domain/auctionItem/service/MyAuctionItemService.java
+++ src/main/java/io/dcns/wantitauction/domain/auctionItem/service/MyAuctionItemService.java
@@ -5,7 +5,6 @@ import io.dcns.wantitauction.domain.auctionItem.dto.MyAuctionItemsResponseDto;
 import io.dcns.wantitauction.domain.auctionItem.entity.AuctionItem;
 import io.dcns.wantitauction.domain.auctionItem.repository.AuctionItemRepository;
 import io.dcns.wantitauction.domain.user.entity.User;
-import java.util.ArrayList;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -22,12 +21,10 @@ public class MyAuctionItemService {
 
     public List<MyAuctionItemsResponseDto> getAuctionItems(Long userId) {
 
-        List<MyAuctionItemsResponseDto> response = new ArrayList<>();
         List<AuctionItem> auctionItems = auctionItemRepository.findAllByUserId(userId);
 
-        for (AuctionItem auctionItem : auctionItems) {
-            response.add(new MyAuctionItemsResponseDto(auctionItem));
-        }
-        return response;
+        return auctionItems.stream()
+            .map(MyAuctionItemsResponseDto::new)
+            .toList();
     }
 }
