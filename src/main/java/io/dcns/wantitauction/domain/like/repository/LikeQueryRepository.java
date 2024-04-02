9556b2b601bfad319cd80f3a8623350b13102d2e
YOOSUNAH <tjsdkyd4027@gmail.com>
2024-04-02 15:55:54 +0900

    ♻️ recycle : 좋아요(찜하기) 등록 및 취소, 조회 부분 리팩토링 (query DSL로 수정 포함)

--- src/main/java/io/dcns/wantitauction/domain/like/repository/LikeQueryRepository.java
+++ src/main/java/io/dcns/wantitauction/domain/like/repository/LikeQueryRepository.java
@@ -1,6 +1,10 @@
 package io.dcns.wantitauction.domain.like.repository;
 
+import static io.dcns.wantitauction.domain.like.entity.QLike.like;
+
 import com.querydsl.jpa.impl.JPAQueryFactory;
+import io.dcns.wantitauction.domain.like.entity.Like;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
@@ -9,4 +13,13 @@ import org.springframework.stereotype.Repository;
 public class LikeQueryRepository {
 
     private final JPAQueryFactory jpaQueryFactory;
+
+    public List<Like> findAllByUserId(Long userId) {
+        return jpaQueryFactory
+            .selectFrom(like)
+            .where(like.userId.eq(userId))
+            .fetch();
+    }
 }
+
+
--- src/main/java/io/dcns/wantitauction/domain/like/service/LikeService.java
+++ src/main/java/io/dcns/wantitauction/domain/like/service/LikeService.java
@@ -1,17 +1,13 @@
 package io.dcns.wantitauction.domain.like.service;
 
-import io.dcns.wantitauction.domain.auctionItem.entity.AuctionItem;
 import io.dcns.wantitauction.domain.auctionItem.repository.AuctionItemRepository;
 import io.dcns.wantitauction.domain.like.dto.LikeResponseDto;
 import io.dcns.wantitauction.domain.like.entity.Like;
+import io.dcns.wantitauction.domain.like.repository.LikeQueryRepository;
 import io.dcns.wantitauction.domain.like.repository.LikeRepository;
 import io.dcns.wantitauction.domain.user.entity.User;
-import io.dcns.wantitauction.domain.user.repository.UserRepository;
-import io.dcns.wantitauction.global.exception.UserNotFoundException;
 import java.util.List;
 import java.util.NoSuchElementException;
-import java.util.Optional;
-import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -21,40 +17,34 @@ public class LikeService {
 
     private final LikeRepository likeRepository;
     private final AuctionItemRepository auctionItemRepository;
-    private final UserRepository userRepository;
+    private final LikeQueryRepository likeQueryRepositor;
 
     public LikeResponseDto likeAuctionItem(Long auctionItemId, User user) {
-        AuctionItem auctionItem = validateAuctionItem(auctionItemId);
-        Optional<Like> alreadyLiked = likeRepository.findByAuctionItemIdAndUserId(auctionItemId,
-            user.getUserId());
+        validateAuctionItem(auctionItemId);
+        Like liked = findByAuctionItemIdAndUserId(auctionItemId, user.getUserId());
 
-        if (alreadyLiked.isPresent()) {
-            Like like = alreadyLiked.get();
-            likeRepository.delete(like);
-            return new LikeResponseDto(user.getUserId(), auctionItem.getAuctionItemId(), false);
+        if (liked != null) {
+            likeRepository.delete(liked);
+            return new LikeResponseDto(user.getUserId(), auctionItemId, false);
         } else {
-            Like like = new Like(user, auctionItemId);
-            likeRepository.save(like);
-            return new LikeResponseDto(user.getUserId(), auctionItem.getAuctionItemId(), true);
+            likeRepository.save(new Like(user, auctionItemId));
+            return new LikeResponseDto(user.getUserId(), auctionItemId, true);
         }
     }
 
     public List<LikeResponseDto> getLikedAuctionItem(Long userId) {
-        User user = getUser(userId);
-        List<Like> likedAuctionItemList = likeRepository.findAllByUserId(user.getUserId());
-
+        List<Like> likedAuctionItemList = likeQueryRepositor.findAllByUserId(userId);
         return likedAuctionItemList.stream().map(
-                like -> new LikeResponseDto(user.getUserId(), like.getAuctionItemId(), true))
-            .collect(Collectors.toList());
+            like -> new LikeResponseDto(userId, like.getAuctionItemId(), true)).toList();
     }
 
-    private AuctionItem validateAuctionItem(Long auctionItemId) {
-        return auctionItemRepository.findById(auctionItemId).orElseThrow(
-            () -> new NoSuchElementException("해당 경매상품을 찾을 수 없습니다."));
+    private void validateAuctionItem(Long auctionItemId) {
+        if (!auctionItemRepository.existsByAuctionItemId(auctionItemId)) {
+            throw new NoSuchElementException("해당 auctionItemId이 존재하지 않습니다.");
+        }
     }
 
-    private User getUser(Long userId) {
-        return userRepository.findByUserId(userId)
-            .orElseThrow(() -> new UserNotFoundException("존재하지 않는 사용자입니다."));
+    private Like findByAuctionItemIdAndUserId(Long auctionItemId, Long userId) {
+        return likeRepository.findByAuctionItemIdAndUserId(auctionItemId, userId);
     }
 }
