b63b8048c5a6ec8e6ca597e4e34a4e0f7f503655
HEEEUN9812 <heeeun9812@gmail.com>
2024-04-04 15:07:25 +0900

    :sparkles: feat : 입찰 조회 기능 추가

--- src/main/java/io/dcns/wantitauction/domain/bid/repository/BidQueryRepository.java
+++ src/main/java/io/dcns/wantitauction/domain/bid/repository/BidQueryRepository.java
@@ -1,6 +1,12 @@
 package io.dcns.wantitauction.domain.bid.repository;
 
+import static io.dcns.wantitauction.domain.bid.entity.QBid.bid;
+
+import com.querydsl.core.types.Projections;
 import com.querydsl.jpa.impl.JPAQueryFactory;
+import io.dcns.wantitauction.domain.bid.dto.BidResponseDto;
+import io.dcns.wantitauction.domain.user.entity.User;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
@@ -9,4 +15,20 @@ import org.springframework.stereotype.Repository;
 public class BidQueryRepository {
 
     private final JPAQueryFactory jpaQueryFactory;
+
+    public List<BidResponseDto> findAllBids(User user) {
+        return jpaQueryFactory
+            .select(Projections.fields(BidResponseDto.class,
+                bid.bidId,
+                bid.auctionItem.auctionItemId,
+                bid.userId,
+                bid.bidPrice,
+                bid.createdAt,
+                bid.updatedAt
+            ))
+            .from(bid)
+            .where(bid.userId.eq(user.getUserId()))
+            .orderBy(bid.createdAt.desc())
+            .fetch();
+    }
 }
