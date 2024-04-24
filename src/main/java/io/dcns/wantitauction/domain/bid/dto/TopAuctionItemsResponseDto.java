2263d2a9b165cecbc7db60e8e6ad0fe00773f42b
HEEEUN9812 <heeeun9812@gmail.com>
2024-04-24 12:07:52 +0900

    :wrench: chore : TopAcutionItemsResponseDto에 category 추가

--- src/main/java/io/dcns/wantitauction/domain/bid/dto/TopAuctionItemsResponseDto.java
+++ src/main/java/io/dcns/wantitauction/domain/bid/dto/TopAuctionItemsResponseDto.java
@@ -1,6 +1,7 @@
 package io.dcns.wantitauction.domain.bid.dto;
 
 import io.dcns.wantitauction.domain.auctionItem.entity.AuctionItem;
+import io.dcns.wantitauction.domain.auctionItem.entity.CategoryEnum;
 import java.time.LocalDateTime;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -12,6 +13,7 @@ public class TopAuctionItemsResponseDto {
     private Long auctionItemId;
     private String itemName;
     private String itemDescription;
+    private CategoryEnum category;
     private LocalDateTime startDate;
     private LocalDateTime endDate;
 
@@ -19,6 +21,7 @@ public class TopAuctionItemsResponseDto {
         this.auctionItemId = auctionItem.getAuctionItemId();
         this.itemName = auctionItem.getItemName();
         this.itemDescription = auctionItem.getItemDescription();
+        this.category = auctionItem.getCategory();
         this.startDate = auctionItem.getStartDate();
         this.endDate = auctionItem.getEndDate();
     }
