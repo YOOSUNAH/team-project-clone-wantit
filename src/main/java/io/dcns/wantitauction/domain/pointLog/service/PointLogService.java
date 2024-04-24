739c46859d6f05b4946c568cf5f3dfcd2d5950e1
HEEEUN9812 <heeeun9812@gmail.com>
2024-04-24 23:07:37 +0900

    :wrench: chore : 코드 수정

--- src/main/java/io/dcns/wantitauction/domain/pointLog/service/PointLogService.java
+++ src/main/java/io/dcns/wantitauction/domain/pointLog/service/PointLogService.java
@@ -4,8 +4,8 @@ import io.dcns.wantitauction.domain.pointLog.dto.PointLogPageableResponseDto;
 import io.dcns.wantitauction.domain.pointLog.dto.PointLogResponseDto;
 import io.dcns.wantitauction.domain.pointLog.repository.PointLogQueryRepository;
 import io.dcns.wantitauction.domain.user.entity.User;
-import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
@@ -18,10 +18,11 @@ public class PointLogService {
 
     public PointLogPageableResponseDto getPointLogs(User user, int page, int size, String status) {
         Pageable pageable = PageRequest.of(page, size);
-        List<PointLogResponseDto> pointLogResponseDtoList = pointLogQueryRepository
-            .findAllPointLogsPageable(user, pageable, status).getContent();
-        int totalPage = pointLogQueryRepository.findAllPointLogsPageable(user, pageable, status)
-            .getTotalPages();
-        return new PointLogPageableResponseDto(pointLogResponseDtoList, size, page + 1, totalPage);
+        Page<PointLogResponseDto> pointLogResponseDtoList = pointLogQueryRepository
+            .findAllPointLogsPageable(user, pageable, status);
+        int totalPage = pointLogResponseDtoList.getTotalPages();
+        return new PointLogPageableResponseDto(
+            pointLogResponseDtoList.getContent(), size, page + 1, totalPage)
+            ;
     }
 }
